e0703663a42cb4d5c8d66d1755da19472e43f6fd
Ary0405 <aryansethia2003@gmail.com>
2022-06-08 17:28:54 +0530

    Screen pages added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:swapsta/screens/explore_screen.dart';
+import 'package:swapsta/screens/profile_screen.dart';
+import 'package:swapsta/screens/swap_screen.dart';
 import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 
 void main() {
@@ -28,11 +31,29 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int currentIndex = 0;
+  late final List<Map<String, dynamic>> _pages;
+  int _pageIndex = 0;
+  void _selectPage(int index) {
+    setState(() {
+      _pageIndex = index;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _pages = [
+      {'page': const ExploreScreen(), 'title': 'Explore'},
+      {'page': const SwapScreen(), 'title': 'Swap'},
+      {'page': const ProfileScreen(), 'title': 'Profile'},
+    ];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(child: Text('Hello')),
+      body: _pages[_pageIndex]['page'],
       bottomNavigationBar: DotNavigationBar(
           dotIndicatorColor: Colors.orange,
           currentIndex: currentIndex,
@@ -40,6 +61,7 @@ class _HomeState extends State<Home> {
           onTap: (index) {
             setState(() {
               currentIndex = index;
+              _pageIndex = index;
             });
           },
           items: [
@@ -48,11 +70,11 @@ class _HomeState extends State<Home> {
               selectedColor: Colors.orange,
             ),
             DotNavigationBarItem(
-              icon: const Icon(Icons.favorite_border),
+              icon: const Icon(Icons.swap_horiz),
               selectedColor: Colors.orange,
             ),
             DotNavigationBarItem(
-              icon: const Icon(Icons.search),
+              icon: const Icon(Icons.account_circle_outlined),
               selectedColor: Colors.orange,
             ),
           ]),
