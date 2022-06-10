9a5fd032f1afd0ac2a6675fa9dbb9187729cb504
shivansh02 <ss398@snu.edu.in>
2022-06-11 03:02:26 +0530

    half stars in condition fixed

--- lib/widgets/condition.dart
+++ lib/widgets/condition.dart
@@ -14,10 +14,29 @@ class ConditionStars extends StatelessWidget {
         for (var i = 0; i < (rating - 0.5); i++, starCounter++)
           const Icon(Icons.star_sharp, size: 16, color: Colors.orange),
         //print half star
+        //
         if (rating % 1 == 0.5)
           for (var i = 0; i < 1; i++, starCounter++)
-            const Icon(Icons.star_half, size: 16, color: Colors.orange),
-        //print black star
+            ShaderMask(
+              blendMode: BlendMode.srcATop,
+              shaderCallback: (Rect rect) {
+                return LinearGradient(
+                  stops: const [0, 0.5, 0.5],
+                  colors: [
+                    Colors.orange,
+                    Colors.orange,
+                    Colors.grey.withOpacity(1)
+                  ],
+                ).createShader(rect);
+              },
+              child: SizedBox(
+                width: 16,
+                height: 16,
+                child:
+                    Icon(Icons.star_sharp, size: 16, color: Colors.grey[300]),
+              ),
+            ),
+        //print blank star
         for (var i = 0; i < 5 && starCounter < 5; i++, starCounter++)
           const Icon(Icons.star_sharp, size: 16, color: Colors.grey),
       ],
