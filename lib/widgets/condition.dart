d68f018a02ee3d8e363a72a3fe62e1bb79a7a3e1
shivansh02 <ss398@snu.edu.in>
2022-06-11 02:46:27 +0530

    half orange half white stars in condition

--- lib/widgets/condition.dart
+++ lib/widgets/condition.dart
@@ -10,15 +10,26 @@ class ConditionStars extends StatelessWidget {
     int starCounter = 0;
     return Row(
       children: [
-        for (var i = 0; i < rating; i++, starCounter++)
-          const Icon(
-            Icons.star_sharp,
-            size: 16,
-            color: Colors.orange,
-          ),
+        //print filled stars
+        for (var i = 0; i < (rating - 0.5); i++, starCounter++)
+          const Icon(Icons.star_sharp, size: 16, color: Colors.orange),
+        //print half star
+        if (rating % 1 == 0.5)
+          for (var i = 0; i < 1; i++, starCounter++)
+            const Icon(Icons.star_half, size: 16, color: Colors.orange),
+        //print black star
         for (var i = 0; i < 5 && starCounter < 5; i++, starCounter++)
           const Icon(Icons.star_sharp, size: 16, color: Colors.grey),
       ],
     );
   }
 }
+
+// Widget getWidget(double rating, int starCounter) {
+//   if (rating % 1 == 0.5) {
+//     starCounter++;
+//     return const Icon(Icons.star_half, size: 16, color: Colors.orange);
+//   } else {
+//     return Text('');
+//   }
+// }
