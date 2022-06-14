7d52508764a338e0943ad4a4e1d40285387e9640
Gautam Nanda <gn567@snu.edu.in>
2022-06-15 03:21:08 +0530

    profile header

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/auth_provider.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Profile Screen'));
+    final user = Provider.of<Auth>(context);
+    return Container(
+      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * .03),
+      height: MediaQuery.of(context).size.height * .1,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 15),
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(user.imageUrl),
+                  radius: 45,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 3),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // const SizedBox(
+                    //   height: 8,
+                    // ),
+                    Text(
+                      user.fullName,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        overflow: TextOverflow.fade,
+                      ),
+                      maxLines: 1,
+                      softWrap: false,
+                    ),
+                    Text(
+                      user.email,
+                      style: const TextStyle(
+                        color: Color.fromARGB(131, 0, 0, 0),
+                        fontSize: 13,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    const Spacer(),
+                    Material(
+                      color: Colors.transparent,
+                      child: Ink(
+                        width: MediaQuery.of(context).size.width * 0.32,
+                        // width: 150,
+                        decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.circular(30),
+                          border: Border.all(
+                            color: Colors.orange,
+                            width: 2,
+                          ),
+                        ),
+                        child: InkWell(
+                          borderRadius: BorderRadius.circular(30),
+                          splashColor: const Color.fromRGBO(255, 152, 0, 0.2),
+                          highlightColor: Colors.transparent,
+                          onTap: (() {}),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 5,
+                              vertical: 3,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: const [
+                                      Text(
+                                        'Edit Profile',
+                                        overflow: TextOverflow.fade,
+                                        style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w400,
+                                          color: Colors.orange,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          IconButton(
+            onPressed: () {
+              Scaffold.of(context).openEndDrawer();
+            },
+            icon: const Icon(
+              Icons.settings_outlined,
+              color: Colors.orange,
+              size: 32,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
